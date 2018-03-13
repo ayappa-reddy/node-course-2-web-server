2b640f233c03164c69b74d943c72781ef8e82e76
ayappa <aayappareddy@gmail.com>
2018-03-13 14:41:23 +0530

    creating a projects page

--- server.js
+++ server.js
@@ -60,6 +60,12 @@ app.get('/', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects page'
+    });
+});
+
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page',
